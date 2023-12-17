54778296a3dfb9af24db508baac41fbdbc453628
KingJ89 <janmutewera@gmail.com>
2023-12-17 05:49:15 -0800

    task factors.c ammended successfully

--- factors.c
+++ factors.c
@@ -61,7 +61,7 @@ int main(int argc, char *argv[])
 {
 	if (argc != 2)
 	{
-		fprintf(stderr, "Usage: % <file>\n", argv[0]);
+		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
 	exit(EXIT_FAILURE);
 	}
 
