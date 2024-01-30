f93f9e09dd20c2267c17a30b68c31d6bde4c6475
KingJ89 <janmutewera@gmail.com>
2024-01-30 01:09:41 -0800

    Task 0 factors completed successfully

--- factors.c
+++ factors.c
@@ -1,39 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include "main.h"
 
+
 /**
- * factorize_number - function that factorizez numbers
- * @number: the number to be factorized.
+ * factorize_number - Factorize a given number into a product of smaller numbers.
+ * @number: The number to factorize.
  */
 void factorize_number(int number)
 {
-	int i;
-
 	printf("%d=", number);
-
+	
+	int i;
+	
 	while (number % 2 == 0)
 	{
 		printf("2");
 		number /= 2;
-
+		
 		if (number > 1)
 		{
 			printf("*");
 		}
 	}
-
+	
 	for (i = 3; i <= sqrt(number); i += 2)
 	{
 		while (number % i == 0)
 		{
 			printf("%d", i);
 			number /= i;
-
+			
 			if (number > 1)
 			{
 				printf("*");
 			}
 		}
 	}
+	
 	if (number > 2)
 	{
 		printf("%d", number);
@@ -42,12 +47,35 @@ void factorize_number(int number)
 }
 
 /**
- * main - Entry point
- * @argc: handles arguments
- * @argv: count of arguments
- * Return: return 0
+ * factorize_file - Factorize numbers from a file and print the results.
+ * @file_path: The path to the file containing numbers.
  */
+void factorize_file(char *file_path)
+{
+	FILE *file = fopen(file_path, "r");
+	
+	if (file == NULL)
+	{
+		perror("Error opening file");
+		exit(EXIT_FAILURE);
+	}
+	
+	int number;
+	
+	while (fscanf(file, "%d", &number) != EOF)
+	{
+		factorize_number(number);
+	}
 
+	fclose(file);
+}
+
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array containing the command-line arguments.
+ * Return: 0 on success, non-zero on failure.
+ */
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
@@ -55,9 +83,10 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
-	char *file_path = argv[1];
-
 	
-
-	return (0);
+	char *file_path = argv[1];
+	
+	factorize_file(file_path);
+	return 0;
 }
+
